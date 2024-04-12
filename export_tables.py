12867014730f83f8b5c0754a1dff814bad1f38d7
Monnerat Marc swisstopo <marc.monnerat@swisstopo.ch>
2024-04-12 10:45:47 +0200

    format

--- export_tables.py
+++ export_tables.py
@@ -1,6 +1,7 @@
 import arcpy
 import pandas as pd
 
+
 def arcgis_table_to_df(in_fc, input_fields=None, query=""):
     """Function will convert an arcgis table into a pandas dataframe with an object ID index, and the selected
     input fields using an arcpy.da.SearchCursor.
@@ -17,9 +18,11 @@ def arcgis_table_to_df(in_fc, input_fields=None, query=""):
     else:
         final_fields = available_fields
     print("Intersection:", final_fields)
-    data = [row for row in arcpy.da.SearchCursor(in_fc,final_fields,where_clause=query)]
-    fc_dataframe = pd.DataFrame(data,columns=final_fields)
-    fc_dataframe = fc_dataframe.set_index(OIDFieldName,drop=True)
+    data = [
+        row for row in arcpy.da.SearchCursor(in_fc, final_fields, where_clause=query)
+    ]
+    fc_dataframe = pd.DataFrame(data, columns=final_fields)
+    fc_dataframe = fc_dataframe.set_index(OIDFieldName, drop=True)
     return fc_dataframe
 
 
@@ -29,21 +32,29 @@ aprx = arcpy.mp.ArcGISProject("CURRENT")
 maps = aprx.listMaps()
 basedir = r"H:\code\geocover-examples\datamodel\input"
 
-fields = list(('OBJECTID','UUID','GEOL_CODE', 'GEOL_CODE_INT', 'GERMAN', 'FMAT_LITSTRAT', 'GERMAN','TREE_LEVEL', 'PARENT_REF'))
+fields = list(
+    (
+        "OBJECTID",
+        "UUID",
+        "GEOL_CODE",
+        "GEOL_CODE_INT",
+        "GERMAN",
+        "FMAT_LITSTRAT",
+        "GERMAN",
+        "TREE_LEVEL",
+        "PARENT_REF",
+    )
+)
 
-with pd.ExcelWriter(os.path.join(basedir, f"tables.xlsx")) as writer:  
-  for map in maps:
-        print ('--------------------------------------------------------------')
-        print (map.name)
-        print ('--------------------------------------------------------------')
+with pd.ExcelWriter(os.path.join(basedir, f"tables.xlsx")) as writer:
+    for map in maps:
+        print("--------------------------------------------------------------")
+        print(map.name)
+        print("--------------------------------------------------------------")
         tables = map.listTables()
         for table in tables:
-          print (table.name)
-          df = arcgis_table_to_df(table.name, input_fields=fields)
-          df.to_csv(os.path.join(basedir, f"{table.name}.csv"), index=True) 
-          df.to_json(os.path.join(basedir, f"{table.name}.json"), index=True) 
-          df.to_excel(writer, sheet_name=table.name.upper())
-
-
-
-
+            print(table.name)
+            df = arcgis_table_to_df(table.name, input_fields=fields)
+            df.to_csv(os.path.join(basedir, f"{table.name}.csv"), index=True)
+            df.to_json(os.path.join(basedir, f"{table.name}.json"), index=True)
+            df.to_excel(writer, sheet_name=table.name.upper())
