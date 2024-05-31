70bf4b1ace1d35de0a0934f76d250ce272054650
Marc Mönneråt <procrastinatio@gmail.com>
2024-05-31 15:55:03 +0200

    Export hierachical  tables to excel (#13)

--- scripts/export_tables.py
+++ scripts/export_tables.py
@@ -2,12 +2,24 @@ import arcpy
 import pandas as pd
 import os
 import sys
+import click
 
 import logging
 
 logging.basicConfig(format="%(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
-TREE_TABLES = ['litho', 'litstrat', 'chrono', 'charcat']
+TREE_TABLES = ["GC_LITHO", "GC_LITSTRAT", "GC_CHRONO", "GC_TECTO"]
+
+DEFAULT_WORKSPACE = r"h:/connections/GCOVERP@osa.sde"
+
+try:
+    curdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
+except NameError:
+    curdir = r"H:\code\lg-geology-data-model"
+DEFAULT_OUTPUT_DIR = os.path.abspath(os.path.join(curdir, "exports"))
+
+now = datetime.datetime.now()
+
 
 def arcgis_table_to_df(in_fc, input_fields=None, query=""):
     """Function will convert an arcgis table into a pandas dataframe with an object ID index, and the selected
@@ -33,57 +45,76 @@ def arcgis_table_to_df(in_fc, input_fields=None, query=""):
     return fc_dataframe
 
 
-
-
-aprx = arcpy.mp.ArcGISProject("CURRENT")
-maps = aprx.listMaps()
-try:
-    curdir  = os.path.dirname(os.path.realpath(__file__))
-except NameError:
-    curdir = r'H:\code\geocover-examples\datamodel'
-basedir = os.path.join(curdir, 'exports')
-
-if not  os.path.isdir(basedir):
-    os.makedirs(basedir)
-
-fields = list(
-    (
-        "OBJECTID",
-        "GEOL_CODE",
-        "GEOL_CODE_INT",
-        "GERMAN",
-        "FMAT_LITSTRAT",
-        "GERMAN",
-        "TREE_LEVEL",
-        "PARENT_REF",
-    )
+@click.command(context_settings={"show_default": True})
+@click.option(
+    "-o",
+    "--output-dir",
+    type=click.Path(exists=True, file_okay=False),
+    help="The directory for the output",
+    default=DEFAULT_OUTPUT_DIR,
 )
+@click.option(
+    "-w",
+    "--workspace",
+    type=str,
+    help="Workspace (SDE string or GDB)",
+    default=DEFAULT_WORKSPACE,
+)
+def main(output_dir, workspace):
+    arcpy.env.workspace = workspace
 
-with pd.ExcelWriter(os.path.join(basedir, f"export_tables.xlsx")) as writer:
-    for mp in maps:
-        logging.info("--------------------------------------------------------------")
-        logging.info(mp.name)
-        logging.info("--------------------------------------------------------------")
-        tables = mp.listTables()
+    tables = []
+
+    for table in arcpy.ListTables():
+        if table.endswith("_I"):
+            continue
+        prefix, short_name = table.split(".")
+        if short_name not in TREE_TABLES:
+            continue
+        tables.append((table, short_name.replace('GC_','')))
+
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+
+    fields = list(
+        (
+            "OBJECTID",
+            "GEOL_CODE",
+            "GEOL_CODE_INT",
+            "GERMAN",
+            "FMAT_LITSTRAT",
+            "GERMAN",
+            "TREE_LEVEL",
+            "PARENT_REF",
+        )
+    )
+
+    logging.info(f"Writting to {output_dir}")
+
+    with pd.ExcelWriter(os.path.join(output_dir, f"export_tables.xlsx")) as writer:
         if len(tables) < 1:
             logging.error("No Table found. Exit")
             sys.exit(2)
-        for table in tables:
-            logging.info(table.name)
+        for table_name, short_name in tables:
+            logging.info(table_name)
 
             try:
-                df = arcgis_table_to_df(table.name, input_fields=fields)
-                df['PARENT_REF'] = df['PARENT_REF'].fillna(0)
+                df = arcgis_table_to_df(table_name, input_fields=fields)
+                df["PARENT_REF"] = df["PARENT_REF"].fillna(0)
             except KeyError as e:
-                logging.error(f"Table {table.name} has nokey: {e}")
+                logging.error(f"Table {table_name} has nokey: {e}")
                 continue
 
-            df.sort_values(by=['GEOL_CODE_INT', 'PARENT_REF'], inplace=True)
-            #df = df.reindex(df.columns.union(fields, sort=False), axis=1, fill_value=0)
+            df.sort_values(by=["GEOL_CODE_INT", "PARENT_REF"], inplace=True)
+            # df = df.reindex(df.columns.union(fields, sort=False), axis=1, fill_value=0)
 
             try:
-                df.to_csv(os.path.join(basedir, f"{table.name}.csv"), index=True)
-                df.to_json(os.path.join(basedir, f"{table.name}.json"), index=True)
-                df.to_excel(writer, sheet_name=table.name.upper())
+                df.to_csv(os.path.join(output_dir, f"{short_name}.csv"), index=True)
+                df.to_json(os.path.join(output_dir, f"{short_name}.json"), index=True)
+                df.to_excel(writer, sheet_name=short_name.upper())
             except PermissionError as e:
-                logging.error(f"Permission error: {table}")
+                logging.error(f"Permission error: {table} is probably already opened")
+
+
+if __name__ == "__main__":
+    main()
