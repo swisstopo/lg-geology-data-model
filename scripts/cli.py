5b25d39b93ef5574b619775990dd294a327db844
Monnerat Marc swisstopo <marc.monnerat@swisstopo.ch>
2024-06-04 12:16:09 +0200

    improving

--- scripts/cli.py
+++ scripts/cli.py
@@ -12,6 +12,18 @@ except NameError:
     curdir = r"H:\code\lg-geology-data-model"
 DEFAULT_OUTPUT_DIR = os.path.abspath(os.path.join(curdir, "exports"))
 
+"""
+connection=D:\connections\GCOVERP@osa.sde, datasets=['TOPGIS_GC.GC_ROCK_BODIES', 'TOPGIS_GC.GC_ROCK_BODIES_I', 'GRATICULES_MANAGER.GRATICULES', 'TOPGIS_GC.GC_ERRORS'], feat classes=['TOPGIS_GC.GC_REVISIONSEBENE', 'TOPGIS_GC.GC_CONFLICT_POLYGON']
+
+connection=D:\connections\GCOVERP@osa.sde\TOPGIS_GC.GC_ROCK_BODIES, datasets=[], feat classes=['TOPGIS_GC.GC_EXPLOIT_GEOMAT_PLG', 'TOPGIS_GC.GC_LINEAR_OBJECTS', 'TOPGIS_GC.GC_POINT_OBJECTS', 'TOPGIS_GC.GC_FOSSILS', 'TOPGIS_GC.GC_UNCO_DESPOSIT', 'TOPGIS_GC.GC_BEDROCK', 'TOPGIS_GC.GC_SURFACES', 'TOPGIS_GC.GC_MAPSHEET', 'TOPGIS_GC.GC_EXPLOIT_GEOMAT_PT']
+
+connection=D:\connections\GCOVERP@osa.sde\TOPGIS_GC.GC_ROCK_BODIES_I, datasets=[], feat classes=['TOPGIS_GC.GC_UNCO_DESPOSIT_I', 'TOPGIS_GC.GC_BEDROCK_I', 'TOPGIS_GC.GC_FOSSILS_I', 'TOPGIS_GC.GC_LINEAR_OBJECTS_I', 'TOPGIS_GC.GC_POINT_OBJECTS_I', 'TOPGIS_GC.GC_EXPLOIT_GEOMAT_PT_I', 'TOPGIS_GC.GC_EXPLOIT_GEOMAT_PLG_I', 'TOPGIS_GC.GC_SURFACES_I', 'TOPGIS_GC.GC_MAPSHEET_I']
+
+connection=D:\connections\GCOVERP@osa.sde\GRATICULES_MANAGER.GRATICULES, datasets=[], feat classes=[]
+
+connection=D:\connections\GCOVERP@osa.sde\TOPGIS_GC.GC_ERRORS, datasets=[], feat classes=['TOPGIS_GC.GC_ERRORS_POLYGON', 'TOPGIS_GC.GC_ERRORS_MULTIPOINT', 'TOPGIS_GC.GC_ERRORS_LINE']
+"""
+
 
 @click.command(context_settings={"show_default": True})
 @click.option(
@@ -30,20 +42,31 @@ DEFAULT_OUTPUT_DIR = os.path.abspath(os.path.join(curdir, "exports"))
 )
 def main(output_dir, workspace):
     from encoder import ExtendedEncoder
+    import arcpy
+
+    arcpy.env.workspace = workspace
 
     so = GeocoverSchema.instance(workspace)
 
-    print(so)
     print(so.tables)
 
     encoder = ExtendedEncoder()
-    print(encoder.to_json(so.coded_domains, indent=4))
+    # print(encoder.to_json(so.coded_domains, indent=4))
 
-    print(encoder.to_yaml(so.coded_domains))
+    # print(encoder.to_yaml(so.coded_domains))
 
-    print(so.datasets)
-    print(so.feature_classes)
-    print(encoder.to_yaml(so))
+    # print(so.datasets)
+    # print(so.feature_classes)
+    # print(encoder.to_yaml(so))
+
+    # print(encoder.to_json(so.feature_classes,  indent=4))
+
+    print("##############")
+    print(so.relationships)
+
+    walk = arcpy.da.Walk(datatype="FeatureClass")
+    for root, fds, fcs in walk:
+        print(f"connection={root}, datasets={fds}, feat classes={fcs}\n")
 
 
 if __name__ == "__main__":
--- scripts/schema.py
+++ scripts/schema.py
@@ -4,9 +4,16 @@ import arcpy
 import logging
 
 
-logging.basicConfig(
-    format="%(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
-)
+logging.basicConfig(format="%(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+
+
+EXCLUDE_TABLES = [
+    "GC_REVISIONSEBENE",
+    "GC_CONFLICT_",
+    "GC_ERRORS_",
+    "GC_VERSION",
+    "GC_MAPSHEET",
+]
 
 
 remove_prefix = lambda x: x.split(".")[-1]
@@ -38,10 +45,12 @@ class GeocoverSchema:
 
             self.__tables = []
             self.__coded_domains = []
+            self.__coded_domains_values = {}
             self.__datasets = []
             self.__feature_classes = {}
             self.__feature_class_subtypes = {}
             self.__geol_code = {}
+            self.__relationships = set()
 
     @property
     def tables(self):
@@ -51,13 +60,26 @@ class GeocoverSchema:
 
         return self.__tables
 
-    @property
-    def coded_domains(self):
+    def list_coded_domains(self):
         if len(self.__coded_domains) < 1:
             logging.debug("Collecting coded domains from workspace")
             self.__coded_domains = arcpy.da.ListDomains(self.__workspace)
 
         return self.__coded_domains
+
+    @property
+    def coded_domains(self):
+        if len(self.list_coded_domains()) < 1:
+            self.list_coded_domains()
+        for domain in self.list_coded_domains():
+            if domain.domainType == "CodedValue":
+                coded_values = domain.codedValues
+                domain_dict = {}
+                for val, desc in coded_values.items():
+                    domain_dict[val] = desc
+                self.__coded_domains_values[domain.name] = domain_dict
+
+        return self.__coded_domains_values
 
     @property
     def datasets(self):
@@ -75,7 +97,37 @@ class GeocoverSchema:
             logging.debug("Collecting feature classes from workspace")
             for dataset in self.__datasets:
                 for fc in arcpy.ListFeatureClasses("", "All", dataset):
-                    self.__feature_classes[fc] = {}
+                    logging.debug("\n---{}---".format(fc))
+                    feat_class_dict = {
+                        "name": fc,
+                        "type": "featclass",
+                        "relationships": [],
+                        "fields": [],
+                        "dataset": dataset,
+                    }
+                    fields = arcpy.ListFields(self.__workspace + "/" + str(fc))
+                    for field in fields:
+                        domain = None
+                        if (
+                            len(field.domain) > 0
+                            and field.domain in self.__coded_domains_values
+                        ):
+                            domain = field.domain
+                        logging.debug(
+                            f"    {field.name}, {field.type}, {field.length}, {domain}"
+                        )
+
+                        feat_class_dict["fields"].append(
+                            dict(
+                                {
+                                    "name": field.name,
+                                    "type": field.type,
+                                    "length": field.length,
+                                    "domain": domain,
+                                }
+                            )
+                        )
+                self.__feature_classes[fc] = feat_class_dict
 
         return self.__feature_classes
 
@@ -86,3 +138,19 @@ class GeocoverSchema:
             self.__tables = list(map(remove_prefix, arcpy.ListTables()))
 
         return self.__tables
+
+    @property
+    def relationships(self):
+        if len(self.__relationships) < 1:
+            logging.debug("Collecting relationships from workspace")
+            print(self.feature_classes)
+            for i, fc in enumerate(self.feature_classes.keys()):
+                logging.info(fc)
+                desc = arcpy.Describe(fc)
+                logging.info(desc)
+                for j, rel in enumerate(desc.relationshipClassNames):
+                    relDesc = arcpy.Describe(rel)
+                    logging.debug(relDesc)
+
+                    self.__relationships.add(rel)
+        return self.__relationships
