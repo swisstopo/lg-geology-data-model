73334491f527ae91005f4debd400ddb83460cff0
Marc Mönneråt <procrastinatio@gmail.com>
2024-06-07 12:20:29 +0200

    Script for full export (incomplete) (#16)
    script to dump the ArcSDE schema and export tables needed by the datamodel

---
+++ scripts/geocover.py
@@ -0,0 +1,194 @@
+import datetime
+import json
+import logging
+import os
+import sys
+from collections import OrderedDict
+from copy import deepcopy
+
+import arcpy
+import click
+import pandas as pd
+
+from encoder import ExtendedEncoder
+from schema import GeocoverSchema
+from utils import dump_dict_to_json
+
+# Won't work on h:\
+DEFAULT_WORKSPACE = r"D:/connections/GCOVERP@osa.sde"
+curdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
+DEFAULT_OUTPUT_DIR = os.path.abspath(os.path.join(curdir, "exports"))
+
+
+@click.group(help="Command to work with TOPGIS/GeoCover ArcSDE database")
+def geocover():
+    pass
+
+
+@geocover.command("export", context_settings={"show_default": True}, help="Export some ArcSDE data for use in datamodel")
+@click.option(
+    "-l",
+    "--log-level",
+    default="INFO",
+    type=click.Choice(
+        ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], case_sensitive=True
+    ),
+    show_default=True,
+    help="Log level",
+)
+@click.option(
+    "-o",
+    "--output-dir",
+    type=click.Path(exists=True, file_okay=False),
+    help="The directory for the output",
+    default=DEFAULT_OUTPUT_DIR,
+)
+@click.option(
+    "-w",
+    "--workspace",
+    type=str,
+    help="Workspace (SDE string or GDB)",
+    default=DEFAULT_WORKSPACE,
+)
+def export(output_dir, workspace, log_level):
+    import arcpy
+
+    from encoder import ExtendedEncoder
+
+    now = datetime.datetime.now()
+
+    arcpy.env.workspace = workspace
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(log_level.upper())
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+
+    so = GeocoverSchema.instance(workspace)
+
+    encoder = ExtendedEncoder()
+
+    connection_info = so.connection_info
+    logger.debug(f"Connection info: {connection_info}")
+    base_dict = {
+        "date": now.strftime("%H:%M:%S-%d-%m-%Y"),
+        "database": connection_info,
+    }
+
+    # Export coded domains
+    coded_domains_dict = deepcopy(base_dict)
+
+    coded_domains_dict.update(so.coded_domains)
+
+    logger.info("Writting to 'coded_domains.json'...")
+    dump_dict_to_json(
+        encoder.to_serializable_dict(so.coded_domains),
+        os.path.join(output_dir, "coded_domains.json"),
+    )
+
+    # Export subtypes
+    subtypes_dict = deepcopy(base_dict)
+
+    subtypes_dict.update(so.subtypes)
+
+    dump_dict_to_json(
+        encoder.to_serializable_dict(subtypes_dict),
+        os.path.join(output_dir, "subtypes_dict.json"),
+    )
+
+    logger.info("###### RELATIONSHIPS #########")
+
+    logger.info(encoder.to_serializable_dict(so.relationships))
+
+    # Export tables
+    logger.info("###### TABLES #########")
+    tables_dict = deepcopy(base_dict)
+
+    tables_dict.update(so.tree_tables)
+
+    dump_dict_to_json(
+        encoder.to_serializable_dict(tables_dict),
+        os.path.join(output_dir, "tables.json"),
+    )
+    with pd.ExcelWriter(os.path.join(output_dir, f"export_tables.xlsx")) as writer:
+        for table_name, df in so.tree_tables.items():
+            logger.info(table_name)
+
+            prefix, short_name = table_name.split(".")
+            short_name = short_name.replace("GC_", "").capitalize()
+
+            if set(['PARENT_REf', 'GEOL_CODE_INT']).issubset(df.columns):
+
+              try:
+                df["PARENT_REF"] = df["PARENT_REF"].fillna(0)
+                df.sort_values(by=["GEOL_CODE_INT", "PARENT_REF"], inplace=True)
+              except KeyError as e:
+                logger.error(f"Table {table_name} has nokey: {e}")
+                continue
+
+            logger.info(f"Exporting to {short_name}.csv, {short_name}.json and 'export_tables.xlsx' (sheet={short_name.upper()}")
+            try:
+                df.to_csv(os.path.join(output_dir, f"{short_name}.csv"), index=True)
+                df.to_json(os.path.join(output_dir, f"{short_name}.json"), index=True)
+                df.to_excel(writer, sheet_name=short_name.upper())
+            except PermissionError as e:
+                logger.error(f"Permission error: 'export_tables.xlsx' is probably already opened")
+
+
+@geocover.command("schema", context_settings={"show_default": True}, help="Dumps ArcSDE schema")
+@click.option(
+    "-o",
+    "--output-dir",
+    type=click.Path(exists=True, file_okay=False),
+    help="The directory for the output",
+    default=DEFAULT_OUTPUT_DIR,
+)
+@click.option(
+    "-w",
+    "--workspace",
+    type=str,
+    help="Workspace (SDE string or GDB)",
+    default=DEFAULT_WORKSPACE,
+)
+@click.option(
+    "-l",
+    "--log-level",
+    default="INFO",
+    type=click.Choice(
+        ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], case_sensitive=True
+    ),
+    show_default=True,
+    help="Log level",
+)
+def schema(output_dir, workspace, log_level):
+    arcpy.env.workspace = workspace
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(log_level.upper())
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+
+    so = GeocoverSchema.instance(workspace)
+
+    encoder = ExtendedEncoder()
+
+    connection_info = so.connection_info
+
+    schema_dict = OrderedDict(
+        {
+            "datetime": datetime.datetime.now(),
+            "relationships": so.relationships,
+            "tables": so.tables,
+            "featclasses": so.feature_classes,
+            "domains": so.coded_domains,
+            "subtypes": so.subtypes,
+        }
+    )
+
+    logger.info("Writting to 'geocover-schema-sde.json'...")
+    dump_dict_to_json(
+        encoder.to_serializable_dict(schema_dict),
+        os.path.join(output_dir, "geocover-schema-sde.json"),
+    )
+
+
+if __name__ == "__main__":
+    geocover()
---
+++ scripts/utils.py
@@ -0,0 +1,81 @@
+import json
+import logging
+import os
+
+import arcpy
+import pandas as pd
+
+
+def arcgis_table_to_df(in_fc, input_fields=None, query=""):
+    """Function will convert an arcgis table into a pandas dataframe with an object ID index, and the selected
+    input fields using an arcpy.da.SearchCursor.
+    :param - in_fc - input feature class or table to convert
+    :param - input_fields - fields to input to a da search cursor for retrieval
+    :param - query - sql query to grab appropriate values
+    :returns - pandas.DataFrame"""
+    OIDFieldName = arcpy.Describe(in_fc).OIDFieldName
+    available_fields = [field.name for field in arcpy.ListFields(in_fc)]
+    logging.debug(available_fields)
+    logging.debug(input_fields)
+    if input_fields:
+        final_fields = list(set([OIDFieldName] + input_fields) & set(available_fields))
+    else:
+        final_fields = available_fields
+    logging.debug("Intersection:", final_fields)
+    data = [
+        row for row in arcpy.da.SearchCursor(in_fc, final_fields, where_clause=query)
+    ]
+    fc_dataframe = pd.DataFrame(data, columns=final_fields)
+    fc_dataframe = fc_dataframe.set_index(OIDFieldName, drop=True)
+    return fc_dataframe
+
+
+def dump_dict_to_json(data, file_path):
+    """
+    Dumps a Python dictionary to a JSON file.
+
+    Args:
+        data (dict): The dictionary to be dumped.
+        file_path (str): The path (including the file name) where the JSON file will be saved.
+
+    Raises:
+        TypeError: If data is not a dictionary.
+        ValueError: If the file path is invalid.
+        IOError: If there is an error writing to the file.
+    """
+
+    if not isinstance(data, dict):
+        raise TypeError("data must be a dictionary")
+
+    # Ensure the directory exists
+    directory = os.path.dirname(file_path)
+    if directory and not os.path.exists(directory):
+        try:
+            os.makedirs(directory)
+        except OSError as e:
+            raise ValueError(f"Invalid file path: {e}")
+
+    try:
+        with open(file_path, "w", encoding="utf-8") as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
+    except IOError as e:
+        raise IOError(f"Error writing to file: {e}")
+
+
+def merge_two_dicts(x, y):
+    """Given two dictionaries, merge them into a new dict as a shallow copy."""
+    z = x.copy()
+    z.update(y)
+    return z
+
+
+def get_field_type(stdict):
+    field_type_dict = {}
+    for stkey in list(stdict.keys()):
+        if stkey == "FieldValues":
+            fields = stdict[stkey]
+            for field, fieldvals in list(fields.items()):
+                if not fieldvals[1] is None:
+                    logging.debug(f"   {field}: {fieldvals[1].name} {fieldvals[0]}")
+                    field_type_dict[field] = fieldvals[1].name
+    return field_type_dict
