71d1f0d20608dcbe95b465c20a278de5ad547315
Marc Monnerat <procrastinatio@gmail.com>
2025-08-29 13:54:10 +0200

    replacing setup.py by pyproject.toml

--- setup.py
+++ setup.py
@@ -1,80 +1,13 @@
 #!/usr/bin/env python
-import os
+"""
+Setup script for gcdocs.
+
+Ce fichier est optionnel avec un pyproject.toml moderne, mais peut être utile
+pour la compatibilité avec d'anciens outils ou pour des installations en mode
+développement avec `pip install -e .`
+"""
+
 from setuptools import setup
-from setuptools import find_packages
 
-# read the contents of your README file
-from pathlib import Path
-from distutils.util import convert_path
-
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
-
-with open(os.path.join(this_directory, "VERSION")) as version_file:
-    version = version_file.read().strip()
-
-
-setup(
-    name="geocover",
-    version=version,  # noqa: F821
-    description="Set of tools for generating the geological datamodel "
-    "(https://www.swisstopo.admin.ch/en/geological-model-2d-geocover) ",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="swisstopo",
-    maintainer="Marc Monnerat",
-    maintainer_email="marc.monnerat@swisstopo.ch",
-    url="https://github.com/swisstopo/lg-geology-data-model",
-    license="LICENSE.txt",
-    packages=find_packages(where="src"), 
-    package_dir={"": "src"},
-    include_package_data=True,  # Must be in included in MANIFEST.in
-    package_data={
-        "geocover": ["data/*.csv"],
-    },
-    python_requires=">=3.7",  # ArcGis Pro 3.3 is still 3.7.11
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX :: Linux",
-        "Topic :: Scientific/Engineering :: Image Processing",
-        "Topic :: Scientific/Engineering :: GIS",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-    ],
-    install_requires=[
-        "jinja2",
-        "pyyaml",
-        "click",
-        "geopandas",
-        "pandas",
-        "click",
-        "loguru",
-        "shapely >= 2.0.0",
-        "babel",
-        "jsonschema",
-        "openpyxl",
-        "ruamel.yaml",
-    ],
-    tests_require=["coverage", "unittests"],
-    entry_points={
-        "console_scripts": [
-            "geocover = geocover.geocover:geocover",
-            "datamodel = geocover.datamodel:datamodel",
-        ],
-        "geocover.plugins": [
-            "schema=geocover.geocover:schema",
-            "tables=geocover.geocover:tables",
-            "geolcode=geocover.geocover:geolcode",
-            "filter=geocover.geocover:filter_symbols",
-            "rules=geocover.geocover:export_rules",
-        
-        ],
-    },
-)
+# La configuration est entièrement dans pyproject.toml
+setup()
